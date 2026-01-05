284346712256604ce59f74b8aae297c64b49c109
Harishpsivan <144566678+Harishpsivan@users.noreply.github.com>
2026-01-05 13:39:38 -0500

    redeploy

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://mern-chat-app-1-0dan.onrender.com/", credentials: true }));
 app.use(express.json());
 
 // Auth routes
